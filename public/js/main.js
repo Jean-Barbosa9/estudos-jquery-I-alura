326662d07f030ea21ae9e02950b10d79db39c825
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-01-31 07:36:05 -0200

    aula 2

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,12 @@ tamanhoFrase = frase.split(' ').length,
 numPalavras = $('#numero-palavras');
 
 numPalavras.text(tamanhoFrase);
+
+var campo = $('.campo-digitacao')
+
+campo.on('input', function() {
+  var qtdPalavras = campo.val().split(/\S+/).length - 1
+  var qtdCaracteres = campo.val().replace(/\s+/g,'').length
+  $('#contador-caracteres').text(qtdCaracteres)
+  $('#contador-palavras').text(qtdPalavras)
+})
