c96cba2b7795742b231be50e0860d47d37a70fc8
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-01-31 09:21:15 -0200

    corrigindo a declaração das variáveis

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,28 @@
 // declaração de variáveis
 var frase = $('.frase').text(),
-tamanhoFrase = frase.split(' ').length,
-numPalavras = $('#numero-palavras'),
-campo = $('.campo-digitacao'),
-var tempoRestante = $('#tempo').text();
+  tamanhoFrase = frase.split(' ').length,
+  numPalavras = $('#numero-palavras'),
+  campo = $('.campo-digitacao'),
+  tempoRestante = $('#tempo').text();
 
 // ações
 numPalavras.text(tamanhoFrase);
 
-campo.on('input', function() {
+campo.on('input', function () {
   var qtdPalavras = campo.val().split(/\S+/).length - 1
-  var qtdCaracteres = campo.val().replace(/\s+/g,'').length
+  var qtdCaracteres = campo.val().replace(/\s+/g, '').length
   $('#contador-caracteres').text(qtdCaracteres)
   $('#contador-palavras').text(qtdPalavras)
 });
 
-campo.on('focus', function() {
-  var cronometro = setInterval(function(){
+campo.one('focus', function () {
+  var cronometro = setInterval(function () {
     tempoRestante--;
     $('#tempo').text(tempoRestante)
 
-    if(tempoRestante < 1) {
-      campo.attr('disabled',true)
+    if (tempoRestante < 1) {
+      campo.attr('disabled', true)
       clearInterval(cronometro)
     }
-  },1000)
+  }, 1000)
 });
