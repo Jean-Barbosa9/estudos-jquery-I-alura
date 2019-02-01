42b3c1841b2c35cd230f84c6e7805e4045afc9d3
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-02-01 07:27:03 -0200

    aula 4 atividades 4 e 5

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,49 @@
-// declaração de variáveis
-var frase = $('.frase').text(),
-  tamanhoFrase = frase.split(' ').length,
-  numPalavras = $('#numero-palavras'),
-  campo = $('.campo-digitacao'),
-  tempoRestante = $('#tempo').text();
+// declaração de variáveis globais
+var tempoInicial = $('#tempo').text(),
+  campo = $('.campo-digitacao');
 
-// ações
-numPalavras.text(tamanhoFrase);
+$(function(){
+  atualizaPalavras()
+  inicializaContadores()
+  inicializaCronometro()
+  $('#reiniciar').click(reiniciaJogo);
+})
 
-campo.on('input', function () {
-  var qtdPalavras = campo.val().split(/\S+/).length - 1
-  var qtdCaracteres = campo.val().replace(/\s+/g, '').length
-  $('#contador-caracteres').text(qtdCaracteres)
-  $('#contador-palavras').text(qtdPalavras)
-});
+function atualizaPalavras(){
+  var frase = $('.frase').text(), tamanhoFrase = frase.split(' ').length;
+  numPalavras = $('#numero-palavras');
+  numPalavras.text(tamanhoFrase);
+}
 
-campo.one('focus', function () {
-  var cronometro = setInterval(function () {
-    tempoRestante--;
-    $('#tempo').text(tempoRestante)
+function inicializaContadores(){
+  campo.on('input', function () {
+    var qtdPalavras = campo.val().split(/\S+/).length - 1
+    var qtdCaracteres = campo.val().replace(/\s+/g, '').length
+    $('#contador-caracteres').text(qtdCaracteres)
+    $('#contador-palavras').text(qtdPalavras)
+  });
+}
 
-    if (tempoRestante < 1) {
-      campo.attr('disabled', true)
-      clearInterval(cronometro)
-    }
-  }, 1000)
-});
+function inicializaCronometro(){
+  var tempoRestante = $('#tempo').text();
+
+  campo.one('focus', function () {
+    var cronometro = setInterval(function () {
+      tempoRestante--;
+      $('#tempo').text(tempoRestante)
+
+      if (tempoRestante < 1) {
+        campo.attr('disabled', true)
+        clearInterval(cronometro)
+      }
+    }, 1000)
+  });
+}
+
+function reiniciaJogo() {
+  $('#contador-caracteres').text('0')
+  $('#contador-palavras').text('0')
+  $('#tempo').text(tempoInicial)
+  campo.attr('disabled',false).val('')
+  inicializaCronometro()
+}
