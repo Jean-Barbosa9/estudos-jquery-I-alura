40f30bdccfa3ae210ff38568f0d6b7e343320583
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-02-07 08:19:01 -0200

    iniciando aula 2

---
+++ public/js/frase.js
@@ -0,0 +1,1 @@
+// TODO: criar funções de troca de frase e a que faz o get no servidor
--- servidor/server.js
+++ servidor/server.js
@@ -2,5 +2,6 @@ var http = require('http');
 var app = require('./config/express');
 
 http.createServer(app).listen(3000, function() {
-	console.log('Servidor iniciado');
+  console.log('Servidor iniciado');
+  console.log('abra o navegador na url https://localhost:3000');
 });
