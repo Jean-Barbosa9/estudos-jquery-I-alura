af58bb6c5a91e284bca885ad9221f13ea26e0622
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-02-08 17:08:01 -0200

    correção no console log da url localhost

--- servidor/server.js
+++ servidor/server.js
@@ -3,5 +3,5 @@ var app = require('./config/express');
 
 http.createServer(app).listen(3000, function() {
   console.log('Servidor iniciado');
-  console.log('abra o navegador na url https://localhost:3000');
+  console.log('abra o navegador na url http://localhost:3000');
 });
